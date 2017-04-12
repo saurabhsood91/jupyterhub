c0bd48f895718f37d13c4ef2d90f37a03013a7a1
Saurabh Sood <saurabhsood91@gmail.com>
2017-04-12 15:22:56 -0600

    add custom task

--- setup.py
+++ setup.py
@@ -48,10 +48,10 @@ is_repo = os.path.exists(pjoin(here, '.git'))
 
 def get_data_files():
     """Get data files in share/jupyter"""
-    
+
     data_files = []
     ntrim = len(here + os.path.sep)
-    
+
     for (d, dirs, filenames) in os.walk(share_jupyter):
         data_files.append((
             d[ntrim:],
@@ -133,27 +133,27 @@ def mtime(path):
 class BaseCommand(Command):
     """Dumb empty command because Command needs subclasses to override too much"""
     user_options = []
-    
+
     def initialize_options(self):
         pass
-    
+
     def finalize_options(self):
         pass
-    
+
     def get_inputs(self):
         return []
-    
+
     def get_outputs(self):
         return []
 
 
 class Bower(BaseCommand):
     description = "fetch static client-side components with bower"
-    
+
     user_options = []
     bower_dir = pjoin(static, 'components')
     node_modules = pjoin(here, 'node_modules')
-    
+
     def should_run(self):
         if not os.path.exists(self.bower_dir):
             return True
@@ -166,20 +166,20 @@ class Bower(BaseCommand):
         if not os.path.exists(self.node_modules):
             return True
         return mtime(self.node_modules) < mtime(pjoin(here, 'package.json'))
-    
+
     def run(self):
         if not self.should_run():
             print("bower dependencies up to date")
             return
-        
+
         if self.should_run_npm():
             print("installing build dependencies with npm")
             check_call(['npm', 'install', '--progress=false'], cwd=here)
             os.utime(self.node_modules)
-        
+
         env = os.environ.copy()
         env['PATH'] = npm_path
-        
+
         try:
             check_call(
                 ['bower', 'install', '--allow-root', '--config.interactive=false'],
@@ -197,11 +197,11 @@ class Bower(BaseCommand):
 
 class CSS(BaseCommand):
     description = "compile CSS from LESS"
-    
+
     def should_run(self):
         """Does less need to run?"""
         # from IPython.html.tasks.py
-        
+
         css_targets = [pjoin(static, 'css', 'style.min.css')]
         css_maps = [t + '.map' for t in css_targets]
         targets = css_targets + css_maps
@@ -209,7 +209,7 @@ class CSS(BaseCommand):
             # some generated files don't exist
             return True
         earliest_target = sorted(mtime(t) for t in targets)[0]
-    
+
         # check if any .less files are newer than the generated targets
         for (dirpath, dirnames, filenames) in os.walk(static):
             for f in filenames:
@@ -218,20 +218,20 @@ class CSS(BaseCommand):
                     timestamp = mtime(path)
                     if timestamp > earliest_target:
                         return True
-    
+
         return False
-    
+
     def run(self):
         if not self.should_run():
             print("CSS up-to-date")
             return
-        
+
         self.run_command('js')
-        
+
         style_less = pjoin(static, 'less', 'style.less')
         style_css = pjoin(static, 'css', 'style.min.css')
         sourcemap = style_css + '.map'
-        
+
         env = os.environ.copy()
         env['PATH'] = npm_path
         try:
@@ -248,6 +248,43 @@ class CSS(BaseCommand):
             raise
         # update data-files in case this created new files
         self.distribution.data_files = get_data_files()
+
+
+class SiteCustomizations(BaseCommand):
+    description = "Site level customizations"
+    user_options = [
+        ('css=', None, 'Specify the path to the custom css file'),
+        ('logo=', None, 'Specify the logo to use'),
+    ]
+
+    def initialize_options(self):
+        self.css = None
+        self.logo = None
+
+    def should_run(self):
+        if self.css or self.logo:
+            return True
+        return False
+
+    def run(self):
+        if not self.should_run():
+            print('No Custom CSS or logo')
+            return
+
+        if self.css:
+            if os.path.exists(self.css):
+                css_path = pjoin(static, 'css', 'custom.css')
+                shutil.copyfile(self.css, css_path)
+                print('Copied Custom CSS File')
+            else:
+                print('Custom CSS file does not exist')
+
+        if self.logo:
+            if os.path.exists(self.logo):
+                logo_path = pjoin(static, 'images', 'logo.png')
+                shutil.copyfile(self.logo, logo_path)
+            else:
+                print('Logo does not exist')
 
 
 def js_css_first(cls, strict=True):
@@ -276,6 +313,7 @@ class bdist_egg_disabled(bdist_egg):
 
 
 setup_args['cmdclass'] = {
+    'custom': SiteCustomizations,
     'js': Bower,
     'css': CSS,
     'build_py': js_css_first(build_py, strict=is_repo),
@@ -291,6 +329,7 @@ from setuptools.command.develop import develop
 class develop_js_css(develop):
     def run(self):
         if not self.uninstall:
+            self.distribution.run_command('custom')
             self.distribution.run_command('js')
             self.distribution.run_command('css')
         develop.run(self)
